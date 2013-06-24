5949219aba348b80e0fc23b4bebfc262f3dcbea3
Fabian Giesen <ryg@gmx.net>
2013-06-23 22:03:02 -0700

    Basic blog rendering and linking.

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"html/template"
+        "io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+        "sort"
+)
+
+const (
+        baseUrl string = "http://blog.rygorous.org"
+        numRecentPosts = 5
 )
 
 func Warnf(msg string, args ...interface{}) {
@@ -28,15 +34,50 @@ func readPosts() ([]*Post, error) {
 			return nil, err
 		}
 
-		posts = append(posts, NewPost(file, text))
+                post, err := NewPost(filepath.Base(file), text)
+                if err != nil {
+                        return nil, err
+                }
+
+		posts = append(posts, post)
 	}
 	return posts, nil
 }
 
 type postInfo struct {
-	post   *Post
-	recent []*Post // list of recent posts
-	series []*Post // list of parent posts for series
+	Post   *Post
+        MostRecent *Post
+        Pages []*Post // list of standalone pages
+	Recent []*Post // list of recent posts
+	Series []*Post // list of parent posts for series
+}
+
+type postSortSlice []*Post
+
+func (p postSortSlice) Len() int {
+        return len(p)
+}
+
+func (p postSortSlice) Less(i, j int) bool {
+        return p[i].Id < p[j].Id
+}
+
+func (p postSortSlice) Swap(i, j int) {
+        p[i], p[j] = p[j], p[i]
+}
+
+func reversePosts(posts []*Post) {
+        n := len(posts)
+        for i := 0; i < n/2; i++ {
+                posts[i], posts[n-1-i] = posts[n-1-i], posts[i]
+        }
+}
+
+func max(a, b int) int {
+        if a > b {
+                return a
+        }
+        return b
 }
 
 func processPosts(posts []*Post) error {
@@ -50,28 +91,116 @@ func processPosts(posts []*Post) error {
 		return err
 	}
 
+        // Sort all posts by ID in increasing order
+        sort.Sort(postSortSlice(posts))
+
+        // Determine links between posts.
+        for _, post := range posts {
+                // Determine permalink
+                post.Href = template.URL(post.RenderedName())
+
+                // Determine child linkage from parent linkage
+                if post.Parent != nil {
+                        post.Parent.Kids = append(post.Parent.Kids, post)
+                }
+        }
+
+        // Determine list of "most recent" posts and "series" posts
+        postinfo := new(postInfo)
+        for _, post := range posts {
+                if post.PageName != "" {
+                        // If a post has a page name, it's a standalone page.
+                        postinfo.Pages = append(postinfo.Pages, post)
+                } else {
+                        // Non-standalone pages get indexed.
+                        postinfo.Recent = append(postinfo.Recent, post)
+
+                        // If a post has child posts, it's a series.
+                        if post.Kids != nil {
+                                postinfo.Series = append(postinfo.Series, post)
+                        }
+                }
+        }
+
+        postinfo.Recent = postinfo.Recent[max(len(posts) - numRecentPosts, 0):]
+
+        // Reverse both lists so that most recent elements are in front
+        reversePosts(postinfo.Recent)
+        reversePosts(postinfo.Series)
+
+        postinfo.MostRecent = postinfo.Recent[0]
+
+        // Output files
 	for _, post := range posts {
-		buf := new(bytes.Buffer)
-                tmpl.Execute(buf, &postInfo{
-                        post: post,
-                })
+                fmt.Printf("processing %d: %q\n", post.Id, post.Title)
+
+                outfile, err := os.Create("out/" + post.RenderedName())
+                if err != nil {
+                        return err
+                }
+                defer outfile.Close()
+
+                post.Active = true
+
+                postinfo.Post = post
+                if err = tmpl.Execute(outfile, postinfo); err != nil {
+                        return err
+                }
+
+                post.Active = false
 	}
 
 	return nil
 }
 
-func prepareOutput() {
-	os.Mkdir("out", 0733)
+func copyFile(dstname, srcname string) error {
+        srcf, err := os.Open(srcname)
+        if err != nil {
+                return err
+        }
+        defer srcf.Close()
+
+        dstf, err := os.Create(dstname)
+        if err != nil {
+                return err
+        }
+        defer dstf.Close()
+
+        _, err = io.Copy(dstf, srcf)
+        return err
+}
+
+func prepareOutput() error {
+        // don't check for (expected) error here (directory already exists)
+        os.Mkdir("out", 0733)
+
+        var files []string = []string{
+                "style.less",
+                "less.js",
+        }
+
+        for _, file := range files {
+                err := copyFile("out/" + file, "template/" + file)
+                if err != nil {
+                        return err
+                }
+        }
+
+        return nil
+}
+
+func check(err error) {
+        if err != nil {
+                panic(err)
+        }
 }
 
 func main() {
 	os.Chdir("c:/Store/Blog")
 	posts, err := readPosts()
-	if err != nil {
-		panic(err)
-	}
-	prepareOutput()
-	processPosts(posts)
+        check(err)
+        check(prepareOutput())
+        check(processPosts(posts))
 
 	fmt.Println("Done!")
 }
--- post.go
+++ post.go
@@ -2,28 +2,44 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"github.com/russross/blackfriday"
 	"html/template"
+        "strconv"
+	"strings"
+	"time"
 )
 
 type Post struct {
 	Filename string
-	Id       int
-	Time     string
+	PageName string // name for standalone posts
+	Id       int    // id for regular posts, 0 otherwise
+	Time     time.Time
 	Title    string
 	Content  template.HTML
 	Href     template.URL // permalink
 	Kids     []*Post      // for series
+	Parent   *Post        // for series
 
-        *blackfriday.Html // ugh. but the alternative is implementing all of "Renderer"...
+        Active bool // used during rendering
+
+	*blackfriday.Html // ugh. but the alternative is implementing all of "Renderer"...
 }
 
-func NewPost(filename string, contents []byte) *Post {
+func NewPost(filename string, contents []byte) (*Post, error) {
 	render := blackfriday.HtmlRenderer(
 		blackfriday.HTML_USE_SMARTYPANTS|blackfriday.HTML_SMARTYPANTS_LATEX_DASHES,
 		"", "")
 
-	// TODO parse ID from file name!
+	// attempt to parse ID from file name (if given)
+	id := 0
+        if idx := strings.Index(filename, "-"); idx != -1 {
+                val, err := strconv.Atoi(filename[:idx])
+                if err != nil {
+                        return nil, fmt.Errorf("post %q has an ill-formed ID: %q", filename, filename[:idx])
+                }
+                id = val
+        }
 
 	extensions := 0
 	extensions |= blackfriday.EXTENSION_NO_INTRA_EMPHASIS
@@ -34,13 +50,99 @@ func NewPost(filename string, contents []byte) *Post {
 
 	post := &Post{
 		Filename: filename,
-                Id: 1,
+		Id:       id,
+		// TODO: Series linkage
 		Html: render.(*blackfriday.Html),
 	}
 
-	post.Content = template.HTML(blackfriday.Markdown(contents, post, extensions))
+	err := post.parseContent(contents, extensions)
+	if err != nil {
+		return nil, err
+	}
 
-	return post
+	return post, nil
+}
+
+func (post *Post) parseContent(contents []byte, extensions int) error {
+	rest := contents
+
+	// Lines at the beginning of the file that start with "-" denote property
+	// assignments, which are of the form "<key>=<value>".
+	for rest[0] == '-' {
+		var line string
+		var err error
+
+		eol := bytes.IndexByte(rest, '\n')
+		if eol != -1 {
+			line = string(rest[1:eol])
+			rest = rest[eol+1:]
+		} else {
+			line = string(rest)
+			rest = rest[len(rest):]
+		}
+
+		// if this line was terminated by CRLF, strip the CR too
+		if len(line) > 0 && line[len(line)-1] == '\r' {
+			line = line[:len(line)-1]
+		}
+
+		key, value := parseKeyValueLine(line)
+		if key == "" {
+			return fmt.Errorf("configuration line %q ill-formed in post %q", line, post.Filename)
+		}
+
+		switch key {
+		case "time":
+			post.Time, err = time.Parse("2006-01-02", value)
+			if err != nil {
+				return fmt.Errorf("while trying to parse time in %q: %q", post.Filename, err.Error())
+			}
+
+                case "pagename":
+                        post.PageName = value
+
+		default:
+			return fmt.Errorf("unknown property %q in post %q", key, post.Filename)
+		}
+	}
+
+	post.Content = template.HTML(blackfriday.Markdown(rest, post, extensions))
+	return post.validate()
+}
+
+func (post *Post) validate() error {
+	if post.Id != 0 {
+		if post.Time.IsZero() {
+			return fmt.Errorf("post %q doesn't have a time set", post.Filename)
+		}
+	} else {
+		if post.PageName == "" {
+			return fmt.Errorf("post %q has neither an ID nor a page name", post.Filename)
+		}
+	}
+
+	return nil
+}
+
+func parseKeyValueLine(line string) (key string, value string) {
+	key = ""
+	value = ""
+	idx := strings.Index(line, "=")
+	if idx != -1 {
+		key = line[:idx]
+		value = line[idx+1:]
+	}
+	return
+}
+
+func (p *Post) RenderedName() string {
+	if p.Id != 0 {
+		return fmt.Sprintf("p%d.html", p.Id)
+	} else if p.PageName != "" {
+		return fmt.Sprintf("p%s.html", p.PageName)
+	}
+
+	return ""
 }
 
 // ---- Bunch of functions here to implement the Renderer interface
@@ -60,4 +162,3 @@ func (p *Post) Header(out *bytes.Buffer, text func() bool, level int) {
 	p.Title = string(out.Bytes()[marker:])
 	out.Truncate(marker)
 }
-
