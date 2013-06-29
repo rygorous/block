081959f9552561348612bfa3aaf339c8f5761cfc
Fabian Giesen <ryg@gmx.net>
2013-06-29 01:11:54 -0700

    Prev/Next links; go fmt

--- main.go
+++ main.go
@@ -3,18 +3,18 @@ package main
 import (
 	"fmt"
 	"html/template"
-        "io"
+	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 )
 
 const (
-        baseUrl string = "http://blog.rygorous.org"
-        numRecentPosts = 5
+	baseUrl        string = "http://blog.rygorous.org"
+	numRecentPosts        = 5
 
-        templateDir = "template"
-        outDir = "out"
+	templateDir = "template"
+	outDir      = "out"
 )
 
 func Warnf(msg string, args ...interface{}) {
@@ -36,10 +36,10 @@ func readPosts() ([]*Post, error) {
 			return nil, err
 		}
 
-                post, err := NewPost(filepath.Base(file), text)
-                if err != nil {
-                        return nil, err
-                }
+		post, err := NewPost(filepath.Base(file), text)
+		if err != nil {
+			return nil, err
+		}
 
 		posts = append(posts, post)
 	}
@@ -47,25 +47,28 @@ func readPosts() ([]*Post, error) {
 }
 
 type postInfo struct {
-	Post   *Post
-        MostRecent *Post
-        Pages []*Post // list of standalone pages
-	Recent []*Post // list of recent posts
-	Series []*Post // list of parent posts for series
+	Post *Post
+	Next *Post
+	Prev *Post
+
+	MostRecent *Post
+	Pages      []*Post // list of standalone pages
+	Recent     []*Post // list of recent posts
+	Series     []*Post // list of parent posts for series
 }
 
 func reversePosts(posts []*Post) {
-        n := len(posts)
-        for i := 0; i < n/2; i++ {
-                posts[i], posts[n-1-i] = posts[n-1-i], posts[i]
-        }
+	n := len(posts)
+	for i := 0; i < n/2; i++ {
+		posts[i], posts[n-1-i] = posts[n-1-i], posts[i]
+	}
 }
 
 func max(a, b int) int {
-        if a > b {
-                return a
-        }
-        return b
+	if a > b {
+		return a
+	}
+	return b
 }
 
 func processPosts(posts []*Post) error {
@@ -79,118 +82,129 @@ func processPosts(posts []*Post) error {
 		return err
 	}
 
-        err = LinkPosts(posts)
-        if err != nil {
-                return err
-        }
+	err = LinkPosts(posts)
+	if err != nil {
+		return err
+	}
 
-        // Determine list of "most recent" posts and "series" posts
-        postinfo := new(postInfo)
-        for _, post := range posts {
-                if post.PageName != "" {
-                        // If a post has a page name, it's a standalone page.
-                        postinfo.Pages = append(postinfo.Pages, post)
-                } else {
-                        // Non-standalone pages get indexed.
-                        postinfo.Recent = append(postinfo.Recent, post)
-
-                        // If a post has child posts, it's a series.
-                        if post.Kids != nil {
-                                postinfo.Series = append(postinfo.Series, post)
-                        }
-                }
-        }
-
-        postinfo.Recent = postinfo.Recent[max(len(posts) - numRecentPosts, 0):]
-
-        // Reverse both lists so that most recent elements are in front
-        reversePosts(postinfo.Recent)
-        reversePosts(postinfo.Series)
-
-        postinfo.MostRecent = postinfo.Recent[0]
-
-        // Output files
+	// Determine list of "most recent" posts and "series" posts
+	postinfo := new(postInfo)
 	for _, post := range posts {
-                fmt.Printf("processing %d: %q\n", post.Id, post.Title)
+		if post.PageName != "" {
+			// If a post has a page name, it's a standalone page.
+			postinfo.Pages = append(postinfo.Pages, post)
+		} else {
+			// Non-standalone pages get indexed.
+			postinfo.Recent = append(postinfo.Recent, post)
 
-                outfile, err := os.Create(filepath.Join(outDir, post.RenderedName()))
-                if err != nil {
-                        return err
-                }
-                defer outfile.Close()
+			// If a post has child posts, it's a series.
+			if post.Kids != nil {
+				postinfo.Series = append(postinfo.Series, post)
+			}
+		}
+	}
 
-                post.Active = true
+	postinfo.Recent = postinfo.Recent[max(len(posts)-numRecentPosts, 0):]
 
-                postinfo.Post = post
-                if err = tmpl.Execute(outfile, postinfo); err != nil {
-                        return err
-                }
+	// Reverse both lists so that most recent elements are in front
+	reversePosts(postinfo.Recent)
+	reversePosts(postinfo.Series)
 
-                post.Active = false
+	postinfo.MostRecent = postinfo.Recent[0]
+
+	// Output files
+	for idx, post := range posts {
+		fmt.Printf("processing %d: %q\n", post.Id, post.Title)
+
+		outfile, err := os.Create(filepath.Join(outDir, post.RenderedName()))
+		if err != nil {
+			return err
+		}
+		defer outfile.Close()
+
+		post.Active = true
+
+		postinfo.Post = post
+		postinfo.Prev = nil
+		postinfo.Next = nil
+
+		if idx > 0 && posts[idx-1].Id > 0 {
+			postinfo.Prev = posts[idx-1]
+		}
+
+		if idx+1 < len(posts) && posts[idx+1].Id > 0 {
+			postinfo.Next = posts[idx+1]
+		}
+
+		if err = tmpl.Execute(outfile, postinfo); err != nil {
+			return err
+		}
+
+		post.Active = false
 	}
 
 	return nil
 }
 
 func copyFile(dstname, srcname string) error {
-        srcf, err := os.Open(srcname)
-        if err != nil {
-                return err
-        }
-        defer srcf.Close()
+	srcf, err := os.Open(srcname)
+	if err != nil {
+		return err
+	}
+	defer srcf.Close()
 
-        dstf, err := os.Create(dstname)
-        if err != nil {
-                return err
-        }
-        defer dstf.Close()
+	dstf, err := os.Create(dstname)
+	if err != nil {
+		return err
+	}
+	defer dstf.Close()
 
-        _, err = io.Copy(dstf, srcf)
-        return err
+	_, err = io.Copy(dstf, srcf)
+	return err
 }
 
 func prepareOutput() error {
-        err := os.RemoveAll(outDir)
-        if err != nil {
-                return err
-        }
+	err := os.RemoveAll(outDir)
+	if err != nil {
+		return err
+	}
 
-        // Copy all files from the template dir, except for the actual template html.
-        err = filepath.Walk(templateDir, func(path string, info os.FileInfo, err error) error {
-                relpath, err := filepath.Rel(templateDir, path)
-                if err != nil {
-                        return err
-                }
+	// Copy all files from the template dir, except for the actual template html.
+	err = filepath.Walk(templateDir, func(path string, info os.FileInfo, err error) error {
+		relpath, err := filepath.Rel(templateDir, path)
+		if err != nil {
+			return err
+		}
 
-                if relpath == "template.html" {
-                        return nil
-                }
+		if relpath == "template.html" {
+			return nil
+		}
 
-                outpath := filepath.Join(outDir, relpath)
-                if info.IsDir() {
-                        err = os.MkdirAll(outpath, 0733)
-                } else {
-                        err = copyFile(outpath, path)
-                }
+		outpath := filepath.Join(outDir, relpath)
+		if info.IsDir() {
+			err = os.MkdirAll(outpath, 0733)
+		} else {
+			err = copyFile(outpath, path)
+		}
 
-                return err
-        })
+		return err
+	})
 
-        return err
+	return err
 }
 
 func check(err error) {
-        if err != nil {
-                panic(err)
-        }
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
 	os.Chdir("c:/Store/Blog")
 	posts, err := readPosts()
-        check(err)
-        check(prepareOutput())
-        check(processPosts(posts))
+	check(err)
+	check(prepareOutput())
+	check(processPosts(posts))
 
 	fmt.Println("Done!")
 }
