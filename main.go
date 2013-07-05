43451643360c591b5fffb01ee3386c9838ad6063
Fabian Giesen <ryg@gmx.net>
2013-07-05 01:16:11 -0700

    Initial Atom support. (Still a bit rough)

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/xml"
 	"fmt"
 	"html/template"
 	"io"
@@ -11,12 +12,17 @@ import (
 	"sort"
 	"time"
 
-	_ "code.google.com/p/go.blog/pkg/atom"
+	"code.google.com/p/go.blog/pkg/atom"
 )
 
 type Blog struct {
 	// Configuration options
+	Title          string
+	Tagline        string
+	Hostname       string
 	Url            string
+	Author         string
+	AtomFeedFile   string
 	NumRecentPosts int
 	NumFeedPosts   int
 	MaxImageWidth  int // if images are wider than this, build a thumbnail.
@@ -25,16 +31,16 @@ type Blog struct {
 	OutDir         string
 
 	// Posts
-	AllPosts   []*Post // master list of all posts in the blog
-	MostRecent *Post   // most recently added post
-	Pages      []*Post // standalone pages
-	Recent     []*Post // list of recent posts
-	Series     []*Post // list of parent posts for series
+	AllPosts    []*Post // master list of all posts in the blog (includes regular posts and special pages)
+	MostRecent  *Post   // most recently added post
+	Pages       []*Post // standalone pages
+	PostsByDate []*Post // posts sorted by date (this is really only posts, not standalone pages)
+	Series      []*Post // list of parent posts for series
 
 	// Files
 	files map[string]string // dst_path (relative to output) -> src_path (relative to blog root)
 
-	atomFeed string
+	atomFeed []byte
 }
 
 func Warnf(msg string, args ...interface{}) {
@@ -49,12 +55,11 @@ func (p postsById) Len() int           { return len(p) }
 func (p postsById) Less(i, j int) bool { return p[i].Id < p[j].Id }
 func (p postsById) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 
-func reversePosts(posts []*Post) {
-	n := len(posts)
-	for i := 0; i < n/2; i++ {
-		posts[i], posts[n-1-i] = posts[n-1-i], posts[i]
-	}
-}
+type postsByPublishDate []*Post
+
+func (p postsByPublishDate) Len() int           { return len(p) }
+func (p postsByPublishDate) Less(i, j int) bool { return p[i].Published.After(p[j].Published) }
+func (p postsByPublishDate) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 
 func max(a, b int) int {
 	if a > b {
@@ -118,7 +123,7 @@ func (blog *Blog) LinkPosts() error {
 			blog.Pages = append(blog.Pages, post)
 		} else {
 			// Non-standalone pages get indexed.
-			blog.Recent = append(blog.Recent, post)
+			blog.PostsByDate = append(blog.PostsByDate, post)
 
 		}
 
@@ -134,22 +139,19 @@ func (blog *Blog) LinkPosts() error {
 
 	}
 
+	// Sort posts by date
+	sort.Sort(postsByPublishDate(blog.PostsByDate))
+
 	// Second pass: index series
-	for _, post := range blog.AllPosts {
+	for _, post := range blog.PostsByDate {
 		// If a post has child posts, it's a series.
 		if post.Kids != nil {
 			blog.Series = append(blog.Series, post)
 		}
 	}
 
-	blog.Recent = blog.Recent[max(len(blog.Recent)-blog.NumRecentPosts, 0):]
-
-	// Reverse both lists so that most recent elements are in front
-	reversePosts(blog.Recent)
-	reversePosts(blog.Series)
-
-	if len(blog.Recent) > 0 {
-		blog.MostRecent = blog.Recent[0]
+	if len(blog.PostsByDate) > 0 {
+		blog.MostRecent = blog.PostsByDate[0]
 	}
 
 	return nil
@@ -166,10 +168,11 @@ func (blog *Blog) FindPostById(which PostID) *Post {
 }
 
 type postInfo struct {
-	Post *Post
-	Next *Post
-	Prev *Post
-	Blog *Blog
+	Post   *Post
+	Next   *Post
+	Prev   *Post
+	Blog   *Blog
+	Recent []*Post
 }
 
 func (blog *Blog) WriteOutput() error {
@@ -179,7 +182,39 @@ func (blog *Blog) WriteOutput() error {
 			return err
 		}
 	}
+	blog.renderAtomFeed()
 
+	// Wipe existing output dir
+	if err := os.RemoveAll(blog.OutDir); err != nil {
+		return err
+	}
+
+	// Static files
+	for dst, src := range blog.files {
+		// Make sure the path exists
+		outPath := filepath.Join(blog.OutDir, filepath.FromSlash(dst))
+		if err := os.MkdirAll(filepath.Dir(outPath), 0733); err != nil {
+			return err
+		}
+
+		// Copy the file
+		if err := copyFile(outPath, src); err != nil {
+			return err
+		}
+	}
+
+	if err := blog.writeOutputPosts(); err != nil {
+		return err
+	}
+
+	if err := blog.writeAtomFeed(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (blog *Blog) writeOutputPosts() error {
 	tmpl_text, err := ioutil.ReadFile(filepath.Join(blog.TemplateDir, "template.html"))
 	if err != nil {
 		return err
@@ -190,29 +225,8 @@ func (blog *Blog) WriteOutput() error {
 		return err
 	}
 
-	// Wipe existing output dir
-	if err = os.RemoveAll(blog.OutDir); err != nil {
-		return err
-	}
+	recent := blog.PostsByDate[:max(len(blog.PostsByDate), blog.NumRecentPosts)]
 
-	// Static files
-	for dst, src := range blog.files {
-		// Make sure the path exists
-		outPath := filepath.Join(blog.OutDir, filepath.FromSlash(dst))
-		if err = os.MkdirAll(filepath.Dir(outPath), 0733); err != nil {
-			return err
-		}
-
-		// Copy the file
-		if err = copyFile(outPath, src); err != nil {
-			return err
-		}
-	}
-
-	return blog.writeOutputPosts(tmpl)
-}
-
-func (blog *Blog) writeOutputPosts(tmpl *template.Template) error {
 	for idx, post := range blog.AllPosts {
 		fmt.Printf("processing %d: %q\n", post.Id, post.Title)
 
@@ -223,8 +237,9 @@ func (blog *Blog) writeOutputPosts(tmpl *template.Template) error {
 		}
 
 		postinfo := postInfo{
-			Post: post,
-			Blog: blog,
+			Post:   post,
+			Blog:   blog,
+			Recent: recent,
 		}
 
 		if idx > 0 && blog.AllPosts[idx-1].Id > 0 {
@@ -256,7 +271,75 @@ func (blog *Blog) writeOutputPosts(tmpl *template.Template) error {
 	return nil
 }
 
+func (blog *Blog) writeAtomFeed() error {
+	outfile, err := os.Create(filepath.Join(blog.OutDir, blog.AtomFeedFile))
+	if err != nil {
+		return err
+	}
+
+	_, err = outfile.Write(blog.atomFeed)
+	outfile.Close()
+	return nil
+}
+
+func summary(post *Post) string {
+	// NYI
+	return ""
+}
+
 func (blog *Blog) renderAtomFeed() error {
+	feed := atom.Feed{
+		Title: blog.Title,
+		ID:    blog.Url + "/block/",
+		Link: []atom.Link{
+			{
+				Rel:  "self",
+				Href: blog.Url + "/" + blog.AtomFeedFile,
+			},
+			{
+				Rel:  "alternate",
+				Href: blog.Url,
+			},
+		},
+		Author: &atom.Person{
+			Name: blog.Author,
+		},
+	}
+
+	var updated time.Time
+	for i, post := range blog.PostsByDate {
+		if i >= blog.NumFeedPosts {
+			break
+		}
+		if post.Updated.After(updated) {
+			updated = post.Updated
+		}
+		e := &atom.Entry{
+			Title: post.Title,
+			ID:    feed.ID + post.AssetPath(),
+			Link: []atom.Link{{
+				Rel:  "alternate",
+				Href: blog.Url + "/" + post.RenderedName(),
+			}},
+			Published: atom.Time(post.Published),
+			Updated:   atom.Time(post.Updated),
+			Summary: &atom.Text{
+				Type: "html",
+				Body: summary(post),
+			},
+			Content: &atom.Text{
+				Type: "html",
+				Body: string(post.Content),
+			},
+		}
+		feed.Entry = append(feed.Entry, e)
+	}
+	feed.Updated = atom.Time(updated)
+	data, err := xml.Marshal(&feed)
+	if err != nil {
+		return err
+	}
+	blog.atomFeed = data
 	return nil
 }
 
@@ -277,32 +360,15 @@ func copyFile(dstname, srcname string) error {
 	return err
 }
 
-type postsByPublishDate []*Post
-
-func (p postsByPublishDate) Len() int           { return len(p) }
-func (p postsByPublishDate) Less(i, j int) bool { return p[i].Published.After(p[j].Published) }
-func (p postsByPublishDate) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
-
 // Generates the "Archive" standalone page and adds it to the blog
 func (blog *Blog) GenerateArchive() error {
-	var archivedPosts []*Post
-
-	// Grab all the ID'ed posts and sort them by date
-	for _, post := range blog.AllPosts {
-		if post.Id != 0 {
-			archivedPosts = append(archivedPosts, post)
-		}
-	}
-
-	sort.Sort(postsByPublishDate(archivedPosts))
-
 	// Generate archive markdown
 	buf := new(bytes.Buffer)
 	buf.WriteString("-pagename=archives\n")
 	buf.WriteString("# Archives\n")
 
 	var prevDate time.Time
-	for _, post := range archivedPosts {
+	for _, post := range blog.PostsByDate {
 		// If the month has changed, print a heading.
 		if post.Published.Year() != prevDate.Year() || post.Published.Month() != prevDate.Month() {
 			buf.WriteString("\n### ")
@@ -346,7 +412,12 @@ func main() {
 
 	// Could (should?) read this from config file.
 	blog := &Blog{
-		Url:            "http://blog.rygorous.org",
+		Title:          "The ryg blog",
+		Tagline:        "When I grow up I'll be an inventor.",
+		Hostname:       "blog.rygorous.org",
+		Url:            "http://blog.rygorous.org/test",
+		Author:         "Fabian 'ryg' Giesen",
+		AtomFeedFile:   "feed.atom.xml",
 		NumRecentPosts: 5,
 		NumFeedPosts:   10,
 		MaxImageWidth:  700,
